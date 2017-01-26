b7038194252376d89d9d49be42a4dfd739681b7b
st4bel <nasak.o@hotmail.de>
2017-01-26 20:51:05 +0100

    ss

--- test.lua
+++ test.lua
@@ -1,18 +1,18 @@
 function OnEvent(event, arg)
 	--OutputLogMessage("event = %s, arg = %s\n", event, arg);
 	EnablePrimaryMouseButtonEvents(true)
-	if (event == "MOUSE_BUTTON_PRESSED" and arg == 6) then
+	if (event == "MOUSE_BUTTON_PRESSED" and arg == 2) then
 		recoil = true
 	end
-  if (event == "MOUSE_BUTTON_RELEASED" and arg == 6) then
+  if (event == "MOUSE_BUTTON_RELEASED" and arg == 2) then
 		recoil = false
 	end
   if recoil then
     if (event == "MOUSE_BUTTON_RELEASED" and arg == 1) then
-		  Sleep(150)
-		  for i=0,50 do
+		  Sleep(100)
+		  for i=0,25 do
 		    Sleep(1)
-			  MoveMouseRelative(0,2)
+			  MoveMouseRelative(0,1)
 		  end
 	  end
   end
